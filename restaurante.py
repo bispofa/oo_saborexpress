3a7840d4118b3175f7a30e408b78339335a38ecd
bispofa <fabiobispoantonio@hotmail.com>
2023-12-30 10:52:16 -0300

    v.1.0

--- restaurante.py
+++ restaurante.py
@@ -1,30 +1,19 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Italiana'
 
-status = 'ativo' if {restaurante_praca.ativo} else 'inativo'
+restaurante_praca = Restaurante('Praça', 'italiana')
+restaurante_pizza = Restaurante('Pizza Place', 'Fast Food')
 
-print(f'restaurante {restaurante_praca.nome} esta {status}')
 
-restaurante_praca.nome = 'Bistro'
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-
-if restaurante_pizza.categoria == 'Fast Food':
-    print ('Categoria validada')
-else:
-    print ('categotia invalida')
-
-restaurante_pizza.ativo = True
-
-print(f'{restaurante_pizza.nome} pertence a categoria {restaurante_pizza.categoria}')
-
-print('teste')
-
+Restaurante.listar_restaurantes()
